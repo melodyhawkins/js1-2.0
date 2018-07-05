264fb8871f566a7e69d0bc57dce862d7201445f4
Melody Hawkins <melodyhawkins@gmail.com>
2018-07-05 12:03:34 -0700

    completed takehome-day1

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,8 @@
-/*
-Day 1 - In Class Notes
-JR - class co-teacher
-Should always check MDN first
-Codepen is better than JBin
-GOOGLE!!!
-Let's Try It - Done.
-Scripting language
-Basic tenents:
-loosley typed language
-DOM manipulator - takes the visual representation and manipulates it;
-Object oriented - basically all programming that you do will usually fall under two categories - object or functional. JS in its best form is object oriented = exact same piece of code can be applied in various places and same result.
-Essential for building a modern dynamic website.
-Console - use to test that it worked.
-Conventions - Best Practices
-camelCasing - variables and functions
-Variables - tells the computer through the browser to save this part of the memory and when i say the name put the value to it.
-once you declare a variable, you need to define it.
+//Takehome Day1
 
-*/
-//DECLARE A VARIABLE
-var firstName;
-firstName = "Melody";//"String" or 'String'
-
-//DEFINE AND DECLARE A VARIABLE
-var lastName = "Hawkins";
-console.log(firstName, lastName);
-
-//REDEFINE THE VARIABLE
-firstName = 'Superman'; //be consistent with use of quotation marks
-console.log(firstName);
-
-/*
-Scoped indenting - Best Practices
-Anytime you see curly brackets, typically inside that curly bracket is a new scope. Because of that you will always have a single tab to start off your code.
-JS Statements should be terminated with a semicolon:
-  definition, declaration, or combination of both
-  console log;
-  function
-ES6 - current fully supported version of JS
-*/
+var myName = "Melody";
+ console.log(myName);
+var classGoal = "to learn JavaScript so I can become a front-end developer for Bitwise.";
+console.log(classGoal);
+var myFavoriteHobby = "reading";
+console.log(myFavoriteHobby);
