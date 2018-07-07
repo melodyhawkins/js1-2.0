ead1c525c01e4d8532c8c20ead238a7c3041ecc8
Melody Hawkins <melodyhawkins@gmail.com>
2018-07-07 16:51:35 -0700

    takehome-day2

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Takehome-Day2
+let firstName = prompt("What is your first name?");
+console.log(firstName);
+let lastName = prompt("What is your last name?");
+alert(lastName);
+let birthday = prompt("What is your birthday?", new Date('10/6/1976'));
+confirm(`Is ${birthday} your birthday?`);
+alert(birthday);
