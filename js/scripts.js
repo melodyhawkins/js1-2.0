cdfd69b51975b4dd0b988571ed5b77a7dc9d6c0e
Melody Hawkins <melodyhawkins@gmail.com>
2018-07-30 19:59:23 -0700

    day8

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,7 @@ body.insertBefore(bodyDiv, script);
 */
 
 //FIZZBUZZ
+/*
 let body = document.querySelector("body");
 let script = document.querySelector("script");
 
@@ -64,7 +65,7 @@ for(let i = 1; i <= 100; i++) {
   }
   body.insertBefore(newP, script);
 }
-
+*/
 //Take Home
 /*
 Write a program that does the following:
@@ -80,11 +81,100 @@ Write a program that does the following:
 //FORMS
 let loginForm = document.forms.login; //b/c we used name notation we can call it by .notation.
 let emailInput = loginForm.email;
+let passwordInput = loginForm.password;
 
+emailInput.addEventListener("keydown", e => {
+  console.log(e.target.value); //e.target is the target of the event, in this instance the email, what is getting typed. 
+  console.log(e.keyCode); //what key did they hit. 
+});
+
+emailInput.addEventListener("keyup", e =>{
+  console.log(this);//what key did they hit
+  e.target.value //what is getting typed in
+});
+
+loginForm.addEventListener("submit", e =>{
+  e.preventDefault(); //before you do whatever the default action is, stop, and do some other stuff first. The most commone one you will see it on is forms. Prevents from submitting and will do whatever you tell it to do. 
+  //...do some checks, submit the form, etc. but you don't want the info in the form anymore. 
+  loginForm.reset();
+});
+
+
+//Learn RegExp!!!!
 //? = after this = query parameter - asking the backend, given this data, give me back something I expect. 
 //& = separates the parameters
 //GET request = way to interact with a server - submit a request and expect a response. the assumption is that you are not actually sending parameters, you are just going to hit a specific URL and it will send you something. Ex. www.google.com. 
 //POST request= way to interact with a server - expects parameters, expects you to ask for something specific and to return something specific to you. Ex. logging into a website. 
+
+//What is "this"
+
+/*
+Refers to how a function gets called. Call site is where you call a function. Call site also refers to the scope that it is in. It is a property of a function and that property does not get defined until you call the function and it is defined at that moment.
+you have an object defining what your user looks like. 
+*/
+// let favorites = ["pickles"];
+function listItems(pet) {
+  console.log(this); //if you don't do anything to change the definition of "this" in a function, it will always default to the window. 
+  alert(`${this.name} loves ${this.favorites[0]} and has a ${pet}.`);
+};
+// listItems();
+
+let user = {
+  name: "Ryeker",
+  cart: [],
+  coupons: [],
+  favorites: ["cheese"],
+  listFavorites: listItems //IMPLICITLY BINDS THIS
+};
+
+//IMPLICIT "THIS" BINDING
+user.listFavorites(); //so, "this" = "user"
+//log out: Ryeker loves cheese;
+
+//EXPLICIT "THIS" BINDING
+
+let jr = {
+  name: "James Royce?",
+  cart: ["guns"],
+  coupons: [1.99],
+  favorites: ["Star Wars"],
+};
+
+//CALL
+listItems.call(jr, "dog"); //Rule 1 about call is it must be called on a function. Rule 2 about call is if the function being bound requires Arguments, they begin as the SECOND argument of the call(). The first argument is always going to be the "this" argument that you are changing. 
+
+//APPLY - explicitly bind
+listItems.apply(jr, ["cat"]);// only difference is the second argument is placed in an array.
+
+
+//SCOPE
+
+
+//Call Stack 
+/*
+way that the engine inside your browser handles all of the different things you are telling it to do with JS. 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 Today we begin the final project in earnest. As such, I won't be going over take homes anymore, they will all pertain directly to your final
